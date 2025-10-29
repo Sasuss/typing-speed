9ac6a05c9182551dc587db67157f353c50b66ad4
Sasus <alexander.klimes@outlook.cz>
2025-10-29 11:57:27 +0100

    first version of handling the input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Timer from "./components/Timer"
-import { useState } from "react"
-import wordList from "./components/WordList";
+import { useState, useEffect, useRef, } from "react"
+//import wordList from "./components/WordList";
 
 function App() {
   const [butswitch, setButSwitch] = useState(false);
@@ -8,12 +8,32 @@ function App() {
     setButSwitch(!butswitch);
     console.log(butswitch);
   }
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+
+
+  var word = "test";
+  useEffect(() => {
+    if (butswitch && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [butswitch]);
+  
+  useEffect(() => {
+    console.log(inputValue);
+  }, [inputValue]);
   /*
           {wordList.map((word, index) => (
             <span key={index} className="mx-1">{word}</span>
           ))}
   */
-  console.log(wordList);
+  //console.log(wordList);
+  useEffect(() => {
+      addEventListener("keydown", (event) => {
+        console.log(event.key);
+      });
+
+  }, []);
 
   return (
     <>
@@ -22,7 +42,11 @@ function App() {
       <div className="min-h-screen min-w-screen flex flex-col items-center">
         <h1 className="mb-5">BRAINROT WPM Test</h1>
         <button onClick={ButtonSwitch} className="w-30">Didi</button>
-        <input type="text" placeholder="didi" name="inputField" id="inputField" className="bg-white text-black mt-3 w-100 h-20 text-2xl p-5" disabled={!butswitch}/>
+        <div className="h-20 w-100 bg-white items-center flex mt-5 text-3xl pl-5">
+          <span className="text-black">{word}</span>
+        </div>
+        
+        <input value={inputValue} onChange={e => setInputValue(e.target.value)} type="text" placeholder="didi" name="inputField" id="inputField" className="bg-white text-black mt-3 w-100 h-20 text-2xl p-5" disabled={!butswitch} ref={inputRef}/>
         {butswitch && <Timer />}
       </div>
     </>
