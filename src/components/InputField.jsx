cb8bb1c380afc02e654ead5f551dd73d7a7bc557
Sasus <alexander.klimes@outlook.cz>
2025-11-06 13:14:24 +0100

    added wrongchar control

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -5,6 +5,7 @@ function InputField() {
   const [inputValue, setInputValue] = useState([]);
   const [wordIndex, setWordIndex] = useState(0);
   const [wordCounter, setWordCounter] = useState(0);
+  const [wrongChars, setWrongChars] = useState(0);
   const wordArray = wordList[wordCounter].split(" ");
   const AllowedKeys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? ";
   const [currentWord, setCurrentWord] = useState(wordList[0]);
@@ -12,7 +13,7 @@ function InputField() {
 
   useEffect(() => {
 
-    if (wordIndex === wordLength) {
+    if (wordIndex === wordLength && wrongChars === 0) {
       setWordCounter((prev) => {
         const nextIndex = prev + 1;
         if (nextIndex < wordList.length) {
@@ -23,7 +24,7 @@ function InputField() {
       setWordIndex(0);
       setInputValue([]);
     }
-  }, [wordIndex, wordLength]);
+  }, [wordIndex, wordLength, wrongChars]);
 
   useEffect(() => {
     const KeyDown = (event) => {
@@ -32,13 +33,21 @@ function InputField() {
       if (event.key === "Backspace") {
         setWordIndex((prev) => (prev > 0 ? prev - 1 : 0));
         setInputValue((prev) => prev.slice(0, -1));
+
+        setWrongChars((prev) => (prev > 0 ? prev - 1 : 0));
+        
         return;
       }
 
       const currentChar = currentWord[wordIndex];
       const typedChar = event.key;
       const isCorrect = typedChar === currentChar;
-
+      
+      if (!isCorrect) {
+        setWrongChars((prev) => prev + 1);
+      }
+      console.log("Current Char:", currentChar, "Typed Char:", typedChar, "Is Correct:", isCorrect);
+      console.log("Wrong Chars:", wrongChars);
       const newSpan = (
         <span
           key={wordIndex}
@@ -54,7 +63,7 @@ function InputField() {
 
     window.addEventListener("keydown", KeyDown);
     return () => window.removeEventListener("keydown", KeyDown);
-  }, [wordIndex, wordArray]);
+  }, [wordIndex, wordArray, currentWord, wrongChars]);
 
   return (
     <div className="h-20 w-100 bg-white items-center flex mt-5 text-3xl pl-5">
